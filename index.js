8a7dd66638e759e731fc74eb7f182e3dbb9a88dc
Vanshdev21 <vanshkhubchandani94@gmail.com>
2026-07-16 19:01:55 +0530

    JWT added

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import { connectDB } from "./config/db.js";
 import User from "./model/User.model.js";
 import { validateSignUpData } from "./utils/validation.js";
 import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import { userAuth } from "./middlewares/auth.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/api/signup", async (req, res) => {
   try {
@@ -39,68 +43,36 @@ app.post("/api/login", async (req, res) => {
       return res.status(404).send("Invalid credentials");
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).send("Invalid credentials");
+    console.log("isPasswordValid:", isPasswordValid);
+    if (isPasswordValid) {
+      // Create JWT token and send it in the response
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d", // Token expiration time
+      });
+      // ADD token to cookies or headers for authentication in future requests
+
+      res.cookie("token", token);
+
+      res.status(200).send("Login successful");
+    } else {
+      throw new Error("Invalid credentials");
     }
-    res.status(200).send("Login successful");
   } catch (error) {
     res.status(500).send("Something Went Wrong!!");
   }
 });
 
-app.get("/api/feed", async (req, res) => {
+app.get("/api/profile", userAuth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.status(200).send(users);
+    const user = req.user; // Access the authenticated user from the request
+    res.status(200).send(user);
   } catch (error) {
     res.status(500).send("Something Went Wrong!!");
   }
 });
 
-app.delete("/api/user/delete", async (req, res) => {
-  const { userId } = req.body;
-  try {
-    const users = await User.findByIdAndDelete(userId);
-    res.status(200).send("User is deleted");
-  } catch (error) {
-    res.status(500).send("Something Went Wrong!!");
-  }
-});
-
-app.patch("/api/user/patch/:userId", async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const data = req.body;
-    const ALLOWED_FIELDS = ["age", "gender", "skills", "photoURL", "bio"];
-    const updates = Object.keys(data);
-    const isValidOperation = updates.every((update) =>
-      ALLOWED_FIELDS.includes(update),
-    );
-
-    if (!isValidOperation) {
-      throw new Error("Invalid updates!");
-    }
-
-    const { age, gender, skills, photoURL, bio } = req.body;
-    await User.findByIdAndUpdate(
-      userId,
-      {
-        age,
-        gender,
-        skills,
-        photoURL,
-        bio,
-        // new: true, // This option returns the updated document
-        //
-      },
-      {
-        runValidators: true, // This option ensures that the update operation respects the schema validators
-      },
-    );
-    res.status(200).send("User is updated");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+app.post("/api/sendConnectionRequest", userAuth, (req, res) => {
+  res.send(`Connection request sent ${req.user}`);
 });
 
 connectDB().then(() => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,26 @@
-function adminAuth(req, res, next) {
-  console.log("Checking Admin.....");
+import jwt from "jsonwebtoken";
+import User from "../model/User.model.js";
 
-  const tokken = "xyz";
-  const isAdmin = tokken === "xyz";
-  if (isAdmin) {
-    console.log("You are Admin");
+async function userAuth(req, res, next) {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send("Unauthorized");
+    }
+    const isTokenValid = await jwt.verify(token, process.env.JWT_SECRET);
+    if (!isTokenValid) {
+      return res.status(401).send("Unauthorized");
+    }
+    const userId = isTokenValid._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    req.user = user; // Attach the user object to the request for further use
     next();
-  } else {
-    return res.status(400).send("You are not a admin");
+  } catch (error) {
+    res.status(500).send("Something Went Wrong!!");
   }
 }
 
-export default adminAuth;
+export { userAuth };
